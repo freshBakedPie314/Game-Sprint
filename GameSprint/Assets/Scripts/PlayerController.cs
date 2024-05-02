ef7f9c7a923328428679322c578e46c83ca53c42
Aditya Rout <adityarout0280@gmail.com>
2024-05-03 00:47:42 +0530

    basic movement and attack

---
+++ GameSprint/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    Rigidbody2D rb;
+    public LayerMask enimies;
+    public Transform attackPoint;
+    public float attackRadius;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 movement = new Vector2(Input.GetAxisRaw("Horizontal") , Input.GetAxisRaw("Vertical"));
+        movement.Normalize();
+        movement = movement * speed;
+
+        rb.velocity = movement;
+
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0f;
+        Vector3 direction = mousePosition - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        rb.SetRotation(angle);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Attack();
+        }
+    }
+
+    private void Attack()
+    {
+        Collider2D[] enimiesHIt = Physics2D.OverlapCircleAll(attackPoint.position, attackRadius, enimies);
+        foreach(Collider2D enenmy in enimiesHIt)
+        {
+            Debug.Log(enenmy.name);
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(attackPoint.position, attackRadius);
+    }
+}
